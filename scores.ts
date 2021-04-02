1411e9c1b459c806609fd4a8012a749ea85386bf
Sebastian Günther <sguenther3141@gmail.com>
2021-04-02 07:42:23 +0200

    init server

---
+++ scores.ts
@@ -0,0 +1,20 @@
+interface HighscoreItem {
+  position: number,
+  time: number,
+  date: number,
+}
+
+const highscores: HighscoreItem[]  = [
+  {
+    position: 1,
+    time: 200,
+    date: 1617214306491,
+  },
+  {
+    position: 2,
+    time: 213,
+    date: 1617214307491,
+  }
+]
+
+module.exports = highscores
