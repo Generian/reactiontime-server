19837b320cbda68b8caf4daf30b5c2b7738337dd
Sebastian Günther <sguenther3141@gmail.com>
2021-04-02 08:33:06 +0200

    add build command

---
+++ dist/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const h = require('./scores');
+var morgan = require('morgan');
+app.use(morgan('tiny', {
+    skip: function (req, res) { return req.method !== 'POST'; }
+}));
+app.use(express.json());
+app.get('/api/highscores', (request, response) => {
+    response.json(h);
+});
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' });
+};
+app.use(unknownEndpoint);
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+//# sourceMappingURL=index.js.map
---
+++ dist/scores.js
@@ -0,0 +1,14 @@
+const highscores = [
+    {
+        position: 1,
+        time: 200,
+        date: 1617214306491,
+    },
+    {
+        position: 2,
+        time: 213,
+        date: 1617214307491,
+    }
+];
+module.exports = highscores;
+//# sourceMappingURL=scores.js.map
